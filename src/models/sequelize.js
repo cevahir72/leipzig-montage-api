d83722632b2f0bb341a96aa25190072e42dc5507
Mevlut KELES <87367047+cevahir72@users.noreply.github.com>
2026-07-05 23:24:25 +0300

    changes

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -4,17 +4,16 @@ require('dotenv').config();
 let sequelize;
 
 try {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-      logging: false,
-    }
-  );
+  sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } catch (error) {
   console.error('Veritabanı bağlantı hatası:', error);
   process.exit(1);
